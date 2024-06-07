121d5fe81a40c467b6d33267ec68af68abac861c
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-07 20:51:13 -0300

    feat: templates

--- TICSolver_flask.py
+++ TICSolver_flask.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import tempfile
 import json
-
 import TICSolver
 
 app = Flask(__name__)
@@ -11,11 +12,30 @@ def index():
     return "waaaa"
 
 
-@app.route('/TICSolver/<rowPag>')
-def TICSolver(rowPag):
-    rowPag = json.loads(rowPag)
-    correct_answers = TICSolver.extract_correct_answers(rowPag)
-    return json.dumps(correct_answers)
+@app.route('/ticsolver', methods=['GET', 'POST'])
+def ticsolver():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+
+            rowpag_data = TICSolver.extract_html_data(filename)
+            os.remove(filename)  # Delete file after processing
+
+            if rowpag_data:
+                correct_answers = TICSolver.extract_correct_answers(rowpag_data)
+                return render_template('results.html', answers=correct_answers)
+            else:
+                return "No data extracted."
+
+    return render_template('upload.html')
 
 
 app.run(host='0.0.0.0', port=9000, ssl_context=(
