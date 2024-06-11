8d6fe694738fc8348e57b3d08b512ec6e548aa1e
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-11 11:58:03 -0300

    feat: New error page

--- server.py
+++ server.py
@@ -19,20 +19,23 @@ def ticsolver():
 @app.route('/results', methods=['POST'])
 def results():
     if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
+        try:
+            file = request.files['file']
+            if file:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(file_path)
 
-            rowpag_data = TICSolver.extract_html_data(file_path)
-            os.remove(file_path)
-            if rowpag_data:
-                correct_answers = TICSolver.extract_correct_answers(rowpag_data)
-                return render_template('results.html', answers=correct_answers)
+                rowpag_data = TICSolver.extract_html_data(file_path)
+                os.remove(file_path)
+                if rowpag_data:
+                    correct_answers = TICSolver.extract_correct_answers(rowpag_data)
+                    return render_template('results.html', answers=correct_answers)
+                else:
+                    return "Error: No data extracted from the file."
             else:
-                return "Error: No data extracted from the file."
-        else:
-            return "Error: No file uploaded."
+                return "Error: No file uploaded."
+        except Exception as e:
+            return render_template('error.html')
 
 
 # app.run(host='0.0.0.0', port=9000, ssl_context=(
